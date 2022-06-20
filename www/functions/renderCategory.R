01a15ca05a7887e298a607ca367acf1bb76badf7
karolkiljandev <karoldev@vosfactures.fr>
2022-06-20 23:59:08 +0200

    cd

--- www/functions/renderCategory.R
+++ www/functions/renderCategory.R
@@ -184,7 +184,7 @@ renderCategory <- function(input, output, category, reactList, staticList){
       inputObj[[1]] <- br()
       inputObj[[2]] <- radioButtons(
         "gender",
-        label = staticList$txt[staticList$txt$text_type == "genderLabel", "text"],
+        label = HTML(staticList$txt[staticList$txt$text_type == "genderLabel", "text"]),
         selected = currGender,
         choices = strsplit(staticList$txt[staticList$txt$text_type == "genders", "text"], ",")[[1]]
       )
@@ -193,7 +193,7 @@ renderCategory <- function(input, output, category, reactList, staticList){
       inputObj[[5]] <- br()
       inputObj[[6]] <- radioButtons(
         "filler",
-        label = staticList$txt[staticList$txt$text_type == "fillerLabel", "text"],
+        label = HTML(staticList$txt[staticList$txt$text_type == "fillerLabel", "text"]),
         selected = currFiller,
         choices = strsplit(staticList$txt[staticList$txt$text_type == "fillers", "text"], ",")[[1]],
       )
@@ -221,7 +221,7 @@ renderCategory <- function(input, output, category, reactList, staticList){
     list(
       if (is.element("catHeader", reactList$txt$text_type)) h4(reactList$txt[reactList$txt$text_type == "catHeader", "text"]),
       if (is.element("instr", reactList$txt$text_type)) h5(reactList$txt[reactList$txt$text_type == "instr", "text"]),
-      if (is.element("longText", reactList$txt$text_type)) p(reactList$txt[reactList$txt$text_type == "longText", "text"]),
+      if (is.element("longText", reactList$txt$text_type)) HTML(reactList$txt[reactList$txt$text_type == "longText", "text"]),
       if (is.element("warning", reactList$txt$text_type)) p(class = "warning", strong(reactList$txt[reactList$txt$text_type == "warning", "text"])),
       if (length(inputObj) > 0) div(class=reactList$settings$css_class, inputObj),
       if (length(notes) > 0) notes,
